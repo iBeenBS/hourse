85cd1471ea5a6e85fd5c2c3bb7c67494439ce8d5
iBeenBS <ivandacruzsilvasantoss@gmail.com>
2026-04-11 19:58:24 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+let moeda = "Reais";
+let simboloMoeda = "R$";
+
+const botoes = document.querySelectorAll('.nav-btn');
+
+botoes.forEach(btn => {
+	btn.addEventListener('click', () => {
+
+		botoes.forEach(b => b.classList.remove('active'));
+
+		btn.classList.add('active');
+	});
+});
+/*
+  window.addEventListener('load', () => {
+ 
+						document.querySelectorAll('.reveal').forEach(el => {
+						el.classList.add('show');
+						});
+					});	*/
+
+
+function setMoedaR() {
+	  moeda = "Reais";
+	  simboloMoeda = "R$";
+}
+function setMoedaT() {
+	  moeda = "Thaisinhas";
+	  simboloMoeda = "T$";
+}
